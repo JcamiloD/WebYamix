efa475e160e3c826fb3089a91c06fbd30cbc7235
Mathiws112 <mathiws112@gmail.com>
2024-11-12 03:26:51 -0500

    por favor no lo dañes camilo

--- routes/asistencia.routes.js
+++ routes/asistencia.routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { verifyToken } = require('../controller/middleware/verificarToken');
+const asistencias = require('../controller/asistenciaController'); 
 
-router.get('/asistencia', (req, res) => {
+router.get('/asistencia', asistencias.traerAsistencia, (req, res) => {
     res.render('./admin/asistencia', { data: res.locals.data });
 });
+
+router.post('/crear_asistencia', asistencias.crearAsistencia)
 
 
 module.exports = router;
