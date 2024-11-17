7411e7aa52a65119eb36c0f0ab5c2131faddf1fb
JuanBeta12 <juaebetancur1@misena.edu.co>
2024-11-16 21:46:52 -0500

    eventos sin id_curso, eventos visualmente funcional

--- routes/calendario.routes.js
+++ routes/calendario.routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const eventos = require('../controller/calendarioController'); // Verifica que la ruta sea correcta
+const eventos = require('../controller/calendarioController'); 
 
 const router = express.Router();
 
 // Rutas para manejar eventos
 router.get('/traer_eventos', eventos.traerEventos);
-router.get('/obtener_evento/:id', eventos.obtenerEventoPorId); // Cambia a obtenerEventoPorId
+router.get('/obtener_evento/:id', eventos.obtenerEventoPorId); 
 router.post('/agregar_evento', eventos.agregarEvento);
 router.post('/actualizar_evento/:id', eventos.actualizarEvento);
 router.delete('/eliminar_evento/:id', eventos.eliminarEvento);
 router.get('/traerEventosPorNombreClase/:nombre_clase', eventos.traerEventosPorNombreCurso); 
 
-// ruta para el calendarioAdmin
+
 router.get('/calendarioAdmin', 
     eventos.obtenerCursoss, 
     (req, res) => {
@@ -22,11 +22,13 @@ router.get('/calendarioAdmin',
     }
 );
     router.get('/modalUpdateEvento', 
-        eventos.obtenerCursoss, 
+        eventos.obtenerCursoss,
+       
         (req, res) => {
         
             res.render('admin/modalUpdateEvento', { 
-                cursos: res.locals.cursoss 
+                cursos: res.locals.cursoss
+               
             });
         }
     );
