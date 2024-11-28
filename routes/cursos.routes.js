9baa3977461117d2254db32b609e9a76ccc55546
JcamiloD <jcamiloarbelaezdiaz@gmail.com>
2024-11-28 16:09:59 -0500

    docs

--- routes/cursos.routes.js
+++ routes/cursos.routes.js
@@ -4,19 +4,23 @@ const router = express.Router();
 
 const cursos = require('../controller/cursosController'); 
 
+const multer = require('multer');
+
+const storage = multer.memoryStorage(); // Usar memoria en lugar de almacenamiento en disco
+const upload = multer({ storage: storage });
+
+
+
+
 // Rutas para los cursos
 
 
-router.post('/agregar_curso', cursos.agregarCurso);
+router.post('/agregar_curso',upload.single('file'), cursos.agregarCurso);
+
 router.put('/actualizar_curso/:id', cursos.actualizarCurso);
 router.delete('/eliminar_curso/:id', cursos.eliminarCurso);
 router.get('/obtener_curso/:id_curso', cursos.obtenerCurso);
 router.get('/traer_cursos', cursos.traerCursos);
-
-module.exports = router;
-
-
-module.exports = router;
 
 router.get('/cursosAdmin', cursos.traerCursos, (req, res) => {
     res.render('admin/cursosAdmin',  { data: res.locals.data});
